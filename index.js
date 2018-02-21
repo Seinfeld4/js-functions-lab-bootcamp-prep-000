cd29d91f104c2be3744e044f0a55e8f834f11bc5
Seth <sdande02@gmail.com>
2018-02-21 08:12:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() {
   return `Happy holidays!`
 }
 
-function happyHolidaysTo() {
-  return `Happy holidays!`
+function happyHolidaysTo(firstName) {
+  return `Happy holidays, ${firstName}`
 }
